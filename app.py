cfc8f404920ec4159e95c1da1ddff8ab76d19a79
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-02-29 10:12:16 +0530

    adding sidebar and progress bar

--- app.py
+++ app.py
@@ -10,106 +10,143 @@ import csv
 from googleapiclient.discovery import build
 import pandas as pd
 import re
+import time
 
-playlist_link = st.text_input("Enter playlist link")
+value = st.sidebar.selectbox(label="Select your Link Type",options=["Playlist","Album"],index=None,placeholder="Select an Option")
 
-submit = st.button('Submit')
+st.title(":musical_note: SPOTIFY")
+st.header("Convert your Spotify Playlist to MP3 Files")
 
-if submit:
+st.write("")
+st.write("")
+st.write("")
 
+if value=='Album':
+    st.write("Album")
 
-    load_dotenv()
-
-    CLIENT_ID = os.getenv("CLIENT_ID")
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
-    client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"    
-    encoded_client_creds = base64.b64encode(client_credentials.encode())
-
-    auth_url = 'https://accounts.spotify.com/api/token'
-
-
-    headers={
-        'Authorization':f"Basic {encoded_client_creds.decode()}"
-    }
-
-    form={
-        'grant_type':'client_credentials'
-    }
-
-    response = requests.post(auth_url,data=form,headers=headers)
+else:
+    form = st.form(key='form_playlist')
+    playlist_link = form.text_input(label="Enter Spotify Playlist Link",placeholder='Eg: https://open.spotify.com/playlist/00i82lDzMDdiHWNjrIGAyw?si=DzmeuZbeRheqRK2DH6R-OA')
+    submit = form.form_submit_button(label='Submit')
 
 
-    if response.status_code==200:
-        access_token=response.json()['access_token']
-        print("sucessful")
-    else:
-        print("error")
-        exit()
+    if submit:
+        load_dotenv()
 
-    os.environ['ACCESS_TOKEN'] = access_token
+        CLIENT_ID = os.getenv("CLIENT_ID")
+        CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-    with open('.env', 'r') as f:
-        lines = f.readlines()
+        client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"    
+        encoded_client_creds = base64.b64encode(client_credentials.encode())
 
-    updated_lines = []
-    found_access_token = False
-    for line in lines:
-        if line.startswith('ACCESS_TOKEN='):
-            updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
-            found_access_token = True
+        auth_url = 'https://accounts.spotify.com/api/token'
+
+
+        headers={
+            'Authorization':f"Basic {encoded_client_creds.decode()}"
+        }
+
+        form={
+            'grant_type':'client_credentials'
+        }
+
+        response = requests.post(auth_url,data=form,headers=headers)
+
+
+        if response.status_code==200:
+            access_token=response.json()['access_token']
+            print("sucessful")
         else:
-            updated_lines.append(line)
+            print("error")
 
-    if not found_access_token:
-        updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
+        os.environ['ACCESS_TOKEN'] = access_token
 
-    with open('.env', 'w') as f:
-        f.writelines(updated_lines)
+        with open('.env', 'r') as f:
+            lines = f.readlines()
+
+        updated_lines = []
+        found_access_token = False
+        for line in lines:
+            if line.startswith('ACCESS_TOKEN='):
+                updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
+                found_access_token = True
+            else:
+                updated_lines.append(line)
+
+        if not found_access_token:
+            updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
+
+        with open('.env', 'w') as f:
+            f.writelines(updated_lines)
 
 
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+        ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 
 
-    uri = playlist_link.split("/")[-1].split("?")[0]
+        uri = playlist_link.split("/")[-1].split("?")[0]
 
 
-    API_KEY=os.getenv("YOUTUBE_API_KEY")
+        # API_KEY=os.getenv("YOUTUBE_API_KEY")
 
-    def get_tracks(playlist_id,access_token):
-        
-        session = spotipy.Spotify(auth=access_token)
-        track = session.playlist_tracks(playlist_id)['items']
-        return track, session
+        def get_tracks(playlist_id,access_token):
+            
+            session = spotipy.Spotify(auth=access_token)
+            track = session.playlist_tracks(playlist_id)['items']
+            return track, session
 
-    def get_video_url(name):
-        youtube=build('youtube','v3',developerKey=API_KEY)
-        request = youtube.search().list(part='snippet',type='video',q=name,maxResults=1)
-        response=request.execute()
-        id = response['items'][0]['id']['videoId']  
-        videoURL=f"https://www.youtube.com/watch?v={id}"  
-        return videoURL
+        # def get_video_url(name):
+        #     youtube=build('youtube','v3',developerKey=API_KEY)
+        #     request = youtube.search().list(part='snippet',type='video',q=name,maxResults=1)
+        #     response=request.execute()
+        #     id = response['items'][0]['id']['videoId']  
+        #     videoURL=f"https://www.youtube.com/watch?v={id}"  
+        #     return videoURL
 
-    tracks,session = get_tracks(uri,ACCESS_TOKEN)
+        tracks,session = get_tracks(uri,ACCESS_TOKEN)
 
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    mp3_directory = os.path.join(script_directory,"downloads_mp3")
-    os.makedirs(mp3_directory, exist_ok=True)
+        # script_directory = os.path.dirname(os.path.abspath(__file__))
+        # mp3_directory = os.path.join(script_directory,"downloads_mp3")
+        # os.makedirs(mp3_directory, exist_ok=True)
 
-    for track in tracks:
-            name = track['track']['name']     
-            artists = ','.join(artist['name'] for artist in track['track']['artists'])  
-            keyword=f"{name},{artists}"  
-            url = get_video_url(keyword)
-            video = YouTube(url)           
-            try:
-                stream = video.streams.filter(only_audio=True).first()
-                title = re.sub(r'[^\w\-_\. ]', '_', video.title)
-                stream.download(output_path=mp3_directory,filename=f"{title}.mp3")
-                print(f"Download of Song {video.title} is completed successfully")
-            except:
-                print("An error has occurred")
-                print(url)
+        progress_text = "Operation in progress. Please wait."
+        p=0
+        bar = st.progress(p,text=progress_text)
+
+        n = len(tracks)
+
+        for track in tracks:
+                # name = track['track']['name']     
+                # artists = ','.join(artist['name'] for artist in track['track']['artists'])  
+                # keyword=f"{name},{artists}"  
+                # url = get_video_url(keyword)
+                # video = YouTube(url)           
+                # try:
+                #     stream = video.streams.filter(only_audio=True).first()
+                #     title = re.sub(r'[^\w\-_\. ]', '_', video.title)
+                #     stream.download(output_path=mp3_directory,filename=f"{title}.mp3")
+                #     print(f"Download of Song {video.title} is completed successfully")
+                # except:
+                #     print("An error has occurred")
+                #     print(url)
+                time.sleep(0.5)
+                bar=bar.progress(p+1,text=progress_text)
+                p+=int(100/n)
+
+        bar=bar.progress(100)
+        st.success("Completed")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # import auth
 # import spotify
