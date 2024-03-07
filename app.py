df53058351daeb703a8c34191ff5096fc84d4f6a
Devansh-Gupta-Offical <03devansh.gupta@gmail.com>
2024-03-07 12:21:37 +0530

    bringing back app.py old version

--- app.py
+++ app.py
@@ -16,20 +16,15 @@ from streamlit_lottie import st_lottie
 from zipfile import ZipFile 
 import shutil
 
-import classify
-import authorization
-import link
-import delete
-import tracks
 
 st.set_page_config(
     page_title="Home",
     page_icon="🎵",
 )
-
-import base64
-import streamlit as st
-
+# st.markdown(""" <style> .font {
+# font-size:50px ; font-family: 'Sans-serif'; color: #F2F2F2;} 
+# </style> """, unsafe_allow_html=True)
+# st.markdown('<h1 class="font">🎵 SPOTIFY ™️</h1>',unsafe_allow_html=True)
 
 #SET Background image to your app
 # def set_bg_hack_url():
@@ -73,22 +68,24 @@ st.write("")
 form = st.form(key='form')
 playlist_link = form.text_input(label="Enter Link",placeholder='Eg: https://open.spotify.com/album/78bpIziExqiI9qztvNFlQu?si=KD20tppwS-udpQqtY38-hA')
 submit = form.form_submit_button(label='Submit')
-
 #CHECKING IF LINK ENTERED IS SPOTIFY OR APPLE MUSIC
-link_type = classify.identify_link(playlist_link)
-    
-st.sidebar.markdown("<h1 style='font-style: italic; color: #F2F2F2;'>Welcome to <span style='font-style: italic; color: #F2F2F2;'>Spotify Downloader!</span></h1>", unsafe_allow_html=True)
+def identify_link(link):
+    if "spotify" in link and "playlist" in link:
+        return "spotify_playlist"
+    elif "spotify" in link and "album" in link:
+        return "spotify_album"
+    else:
+        return "This is not a valid link"
 
-#LOADING ANIMATIONS BASED ON RESULTS
-if link_type=="spotify_playlist" or link_type=="spotify_album":
+st.sidebar.markdown("<h1 style='font-style: italic; color: #F2F2F2;'>Welcome to <span style='font-style: italic; color: #F2F2F2;'>Spotify Downloader!</span></h1>", unsafe_allow_html=True)
     
+#LOADING ANIMATIONS BASED ON RESULTS
+if identify_link(playlist_link)=="spotify_playlist" or identify_link(playlist_link)=="spotify_album":
     #LOADING SPOTIFY ANIMATION
     def load_animations(filepath:str):
         with open(filepath,'r',encoding="utf8") as f:
             return simplejson.load(f)
-
     spotify = load_animations("spotify.json")
-
     with st.sidebar:
         st_lottie(
             spotify,
@@ -102,11 +99,51 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
         )
     
     #GETTING .ENV VARIABLES
-    authorization.get_access_token()
-
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     API_KEY=os.getenv("YOUTUBE_API_KEY")
-
+    #GETTING ACCESS TOKEN
+    load_dotenv()
+    client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"    
+    encoded_client_creds = base64.b64encode(client_credentials.encode())
+    auth_url = 'https://accounts.spotify.com/api/token'
+    headers={
+        'Authorization':f"Basic {encoded_client_creds.decode()}"
+    }
+    form={
+        'grant_type':'client_credentials'
+    }
+    response = requests.post(auth_url,data=form,headers=headers)
+    if response.status_code==200:
+        access_token=response.json()['access_token']
+        print("sucessful")
+    else:
+        access_token=''
+        print("error")
+    os.environ['ACCESS_TOKEN'] = access_token
+    with open('.env', 'r') as f:
+        lines = f.readlines()
+        updated_lines = []
+        found_access_token = False
+        for line in lines:
+            if line.startswith('ACCESS_TOKEN='):
+                updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
+                found_access_token = True
+            else:
+                updated_lines.append(line)
+        if not found_access_token:
+            updated_lines.append(f'ACCESS_TOKEN="{access_token}"\n')
+        with open('.env', 'w') as f:
+            f.writelines(updated_lines)
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    #GET URL OF A SONG FROM YOUTUBE
+    def get_video_url(name):
+            youtube=build('youtube','v3',developerKey=API_KEY)
+            request = youtube.search().list(part='snippet',type='video',q=name,maxResults=1)
+            response=request.execute()
+            id = response['items'][0]['id']['videoId']  
+            videoURL=f"https://www.youtube.com/watch?v={id}"  
+            return videoURL
     #DELETING ZIP FILES CONTENT AFTE RUSAGE
     def delete_files_in_directory(directory_path):
         try:
@@ -118,31 +155,35 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
                 print("All files deleted successfully.")
         except OSError:
             print("Error occurred while deleting files.")
-
-
-
     #IF USER SELECTS ALBUM TYPE LINK
-    if link_type=='spotify_album':
-
+    if identify_link(playlist_link)=='spotify_album':
         #CLEARING DOWNLOADS FOLDER AND ZIP FILE
         script_directory = os.path.dirname(os.path.abspath(__file__))
         zip_to_delete =  os.path.join(script_directory,"mp3.zip")
         directory_to_delete =  os.path.join(script_directory,"downloads")
-
-        delete.delete_files_in_directory(directory_to_delete)
-        delete.delete_files_in_directory(zip_to_delete)
-
+        try:
+            os.remove(zip_to_delete)
+        except:
+            print(f"The specified file does not exist.")
+        try:
+            shutil.rmtree(directory_to_delete)
+        except:
+            print(f"The specified file does not exist.")
+        
+        #GET ALL ALBUM SONGS NAME FROM URI OF LINK
+        def get_album_tracks(album_id,access_token):
+                session = spotipy.Spotify(auth=access_token)
+                track = session.album_tracks(album_id)['items']
+                return track, session
         #IF USER SUBMIT LINKS
         if submit:
             uriAlbum = playlist_link.split("/")[-1].split("?")[0]
             #RUN AND GET NAMES OF SONGS
-            tracksAlbum,session = tracks.get_album_tracks(uriAlbum,ACCESS_TOKEN)
-
+            tracksAlbum,session = get_album_tracks(uriAlbum,ACCESS_TOKEN)
             #INITIALIZE DOWNLOADS FOLDER
             script_directory = os.path.dirname(os.path.abspath(__file__))
             mp3_directory = os.path.join(script_directory,"downloads")
             os.makedirs(mp3_directory, exist_ok=True)
-
             #PROGRESS BAR
             progress_text = "Operation in progress. Please wait."
             p=0
@@ -152,14 +193,13 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
                     album_name = track['name']    #GET SONG NAME
                     album_artists = track['artists'][0]['name']   #GET ARTIST NAME
                     keyword=f"{album_name},{album_artists}"  
-                    url = link.get_video_url(keyword)   #GET YOUTUBE VIDEO LINK
+                    url = get_video_url(keyword)   #GET YOUTUBE VIDEO LINK
                     video = YouTube(url)           
                     try:
                         stream = video.streams.filter(only_audio=True).first()   
                         title = re.sub(r'[^\w\-_\. ]', '_', video.title)    #REMOVE SPECIAL CHARACTERS FROM NAME OF SONG
                         stream.download(output_path=mp3_directory,filename=f"{title}.mp3")  #DOWNLOAD MP3 FROM LINK
                         file_path = rf"{mp3_directory}\{title}.mp3"
-
                         #READING AUDIO FILES TO DISPLAY IN APP
                         audio_file = open(file_path,'rb')
                         audio_read = audio_file.read()
@@ -176,7 +216,6 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
             st.write("")
             st.write("")
             # st.success("Completed")     
-
             #MAKE ZIP FILE
             script_directory = os.path.dirname(os.path.abspath(__file__))
             zip_directory = os.path.join(script_directory,"mp3")
@@ -184,46 +223,39 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
             #ADD DOWNLOADS FOLDER CONTENTS TO ZIP FILE
             with open('mp3.zip', 'rb') as f:
                 zip_album_file=f.read()
-
             #PROVIDE DOWNLOAD BUTTON TO DOWNLOAD ZIP FILE FROM WEB
+                
             col1, col2, col3 = st.columns(3)
             with col2:
                 flag = st.download_button(label='Download Zip', data=zip_album_file, file_name='songs.zip',type="secondary")  # Defaults to 'application/octet-stream'
                 if flag:
                     st.write('Thanks for downloading!')
-
     #IF USER SELECTS PLAYLIST TYPE LINK
-    if link_type=='spotify_playlist':
+    if identify_link(playlist_link)=='spotify_playlist':
         #CLEARING DOWNLOADS FOLDER AND ZIP FILE
         script_directory = os.path.dirname(os.path.abspath(__file__))
         zip_to_delete =  os.path.join(script_directory,"mp3.zip")
         directory_to_delete =  os.path.join(script_directory,"downloads")
-
         try:
             os.remove(zip_to_delete)
         except:
             print(f"The specified file does not exist.")
-
         try:
             shutil.rmtree(directory_to_delete)
         except:
             print(f"The specified file does not exist.")
-
         #GET ALL PLAYLIST SONGS NAME FROM URI OF LINK
         def get_tracks(playlist_id,access_token):
                 session = spotipy.Spotify(auth=access_token)
                 track = session.playlist_tracks(playlist_id)['items']
                 return track, session
-
         #IF USER SUBMITS LINK
         if submit:
             uri = playlist_link.split("/")[-1].split("?")[0]
-            tracks,session = tracks.get_tracks(uri,ACCESS_TOKEN)
-
+            tracks,session = get_tracks(uri,ACCESS_TOKEN)
             script_directory = os.path.dirname(os.path.abspath(__file__))
             mp3_directory = os.path.join(script_directory,"downloads")
             os.makedirs(mp3_directory, exist_ok=True)
-
             progress_text = "Operation in progress. Please wait."
             p=0
             bar = st.progress(p,text=progress_text)
@@ -232,7 +264,7 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
                     name = track['track']['name']     
                     artists = ', '.join(artist['name'] for artist in track['track']['artists'])  
                     keyword=f"{name},{artists}"  
-                    url = link.get_video_url(keyword)
+                    url = get_video_url(keyword)
                     video = YouTube(url)           
                     try:
                         stream = video.streams.filter(only_audio=True).first()
@@ -260,31 +292,24 @@ if link_type=="spotify_playlist" or link_type=="spotify_album":
             shutil.make_archive(zip_directory,'zip',mp3_directory)
             with open('mp3.zip', 'rb') as f:
                 zip_file=f.read()
-
             
             col1, col2, col3 = st.columns([2.5,2,1.5])
             with col2:
                 flag = st.download_button(label='Download Zip', data=zip_file, file_name='songs.zip',type="secondary")  # Defaults to 'application/octet-stream'
                 if flag:
                     st.write('Thanks for downloading!')
-
 else:
     st.warning("Enter a Valid Link")
-
-
-
 #SIDEBAR IMPLEMENTATION
 # ABOUT SECTION
 st.sidebar.title("About")
 st.sidebar.info(
     "Welcome to the Spotify to MP3 Converter App! This app allows you to convert your Spotify playlists or albums into MP3 files. Simply provide the Spotify playlist or album link, and the app will download the corresponding songs in MP3 format."
 )
-
 # HOW TO USE SECTION
 st.sidebar.title("How to Use :open_book:")
 st.sidebar.markdown("1. Choose the type of link you want to convert: Playlist or Album.\n 2. Enter the Spotify link in the respective input field.\n 3. Click the 'Submit' button to start the conversion process.\n 4. Once the conversion is complete, you can listen to a sample of each song before downloading.\n 5. If you're satisfied with the sample, proceed to download the MP3 files in a zip archive.\n"
 )
-
 # ADDITIONAL TIPS SECTION (Optional)
 st.sidebar.title("Tips💡")
 st.sidebar.write(
@@ -292,6 +317,3 @@ st.sidebar.write(
 )
 st.sidebar.write("- Ensure you have a good internet connection for smooth processing.")
 st.sidebar.write("- For any issues or feedback, contact the developer.")
-
-
-
